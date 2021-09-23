c99a23e674b2118281d28c17cbded681c43f5843
David Lougheed <david.lougheed@gmail.com>
2021-09-23 10:25:46 -0400

    more work on station form

--- src/components/stations/StationForm.js
+++ src/components/stations/StationForm.js
@@ -8,8 +8,13 @@ const StationsForm = () => {
 
     const sections = useSelector(state => state.sections.items);
     const categories = useSelector(state => state.categories.items);
+    const assets = useSelector(state => state.assets.items);
 
-    return <Form form={form}>
+    const onFinish = v => {
+        console.log(v);
+    };
+
+    return <Form form={form} layout="vertical" onFinish={onFinish}>
         <Form.Item name="title" label="Title" rules={[{required: true}]}>
             <Input />
         </Form.Item>
@@ -29,11 +34,53 @@ const StationsForm = () => {
         <Form.Item name="category" label="Category" rules={[{required: true}]}>
             <Select placeholder="Select category for this station" options={categories.map(c => ({value: c}))} />
         </Form.Item>
-        <Form.Item name="enabled" label="Enabled" rules={[{required: true}]}>
+        <Form.Item name="header_image" label="Header Image">
+            <Select placeholder="Select header image for this station"
+                    allowClear={true}
+                    options={assets.filter(a => a.asset_type === "image").map(a => ({
+                        value: a.id,
+                        label: a.file_name,
+                    }))} />
+        </Form.Item>
+        <Form.List name="contents">
+            {(fields, {add, remove}, {errors}) => (
+                <>
+                    {fields.map(field => (
+                        <div key={field.key}>
+                            <Form.Item noStyle={true}>
+                                <Form.Item {...field}
+                                           key="content_type"
+                                           label="Content Type"
+                                           name={[field.name, "content_type"]}
+                                           fieldKey={[field.fieldKey, "content_type"]}
+                                           rules={[{required: true}]}>
+                                    <Select placeholder="Content Type" options={[
+                                        {value: "html", label: "Rich Text"},
+                                        {value: "gallery", label: "Gallery"},
+                                        {value: "quiz", label: "Quiz"},
+                                    ]} />
+                                </Form.Item>
+                                <Form.Item {...field}
+                                           key="content_before_fold"
+                                           label="Content Before Fold"
+                                           name={[field.name, "content_before_fold"]}
+                                           fieldKey={[field.fieldKey, "content_before_fold"]}>
+                                    <Input placeholder="TODO" />
+                                </Form.Item>
+                            </Form.Item>
+                        </div>
+                    ))}
+                    <Form.Item>
+                        <Button onClick={() => add()}>Add Content Item</Button>
+                    </Form.Item>
+                </>
+            )}
+        </Form.List>
+        <Form.Item name="enabled" label="Enabled" valuePropName="checked">
             <Switch />
         </Form.Item>
         <Form.Item>
-            <Button type="primary">Submit</Button>
+            <Button type="primary" htmlType="submit">Submit</Button>
         </Form.Item>
     </Form>;
 };
