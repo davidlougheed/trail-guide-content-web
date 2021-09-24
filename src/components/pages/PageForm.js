243f1bf13422042329f5c576545f0d94a8b5e751
David Lougheed <david.lougheed@gmail.com>
2021-09-24 13:15:21 -0400

    add rank to page form

--- src/components/pages/PageForm.js
+++ src/components/pages/PageForm.js
@@ -5,12 +5,20 @@ import {Button, Col, Form, Input, Row, Select, Switch} from "antd";
 
 import HTMLEditor from "../HTMLEditor";
 
-const PageForm = props => {
+const PageForm = ({initialValues, ...props}) => {
     const [form] = Form.useForm();
 
+    const numPages = useSelector(state => state.stations.items.length);
     const assets = useSelector(state => state.assets.items);
 
-    return <Form {...props} form={form} layout="vertical">
+    const oldInitialValues = initialValues ?? {};
+    console.log("initial values", oldInitialValues);
+    const newInitialValues = {
+        rank: numPages,  // Add station to the end of the list in the app by default
+        ...oldInitialValues,
+    };
+
+    return <Form {...props} form={form} layout="vertical" initialValues={newInitialValues}>
         <Row gutter={12}>
             <Col span={8}>
                 <Form.Item name="title" label="Title" rules={[{required: true}]}>
@@ -24,7 +32,7 @@ const PageForm = props => {
             </Col>
         </Row>
         <Row gutter={12}>
-            <Col span={20}>
+            <Col span={16}>
                 <Form.Item name="subtitle" label="Subtitle" rules={[{required: true}]}>
                     <Input />
                 </Form.Item>
@@ -34,19 +42,20 @@ const PageForm = props => {
                     <Switch />
                 </Form.Item>
             </Col>
-        </Row>
-        <Row gutter={12}>
-            <Col span={8}>
-                <Form.Item name="header_image" label="Header Image">
-                    <Select placeholder="Select header image for this station"
-                            allowClear={true}
-                            options={assets.filter(a => a.asset_type === "image").map(a => ({
-                                value: a.id,
-                                label: a.file_name,
-                            }))} />
+            <Col span={4}>
+                <Form.Item name="rank" label="Rank">
+                    <Input type="number" min={0} />
                 </Form.Item>
             </Col>
         </Row>
+        <Form.Item name="header_image" label="Header Image">
+            <Select placeholder="Select header image for this station"
+                    allowClear={true}
+                    options={assets.filter(a => a.asset_type === "image").map(a => ({
+                        value: a.id,
+                        label: a.file_name,
+                    }))} />
+        </Form.Item>
         <Form.Item name="content" label="Content">
             <HTMLEditor />
         </Form.Item>
