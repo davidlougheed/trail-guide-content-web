7a831c4eecb92aacb436019659c3bb9cea7ca787
David Lougheed <david.lougheed@gmail.com>
2021-09-26 20:07:54 -0400

    add settings ui

--- src/components/settings/SettingsPage.js
+++ src/components/settings/SettingsPage.js
@@ -1,7 +1,17 @@
 import React from "react";
+import {PageHeader} from "antd";
+import SettingsForm from "./SettingsForm";
+import {useSelector} from "react-redux";
 
 const SettingsPage = () => {
-    return <div>Settings</div>;
+    const fetchingSettings = useSelector(state => state.settings.isFetching);
+    const settings = useSelector(state => state.settings.data);
+
+    if (fetchingSettings) return <div>Loading...</div>;
+
+    return <PageHeader title="Settings" ghost={false}>
+        <SettingsForm initialValues={settings} />
+    </PageHeader>;
 };
 
 export default SettingsPage;
