5f30b8f8b1fb873756a3a9c90407696eb0e42b1c
David Lougheed <david.lougheed@gmail.com>
2021-09-26 20:07:42 -0400

    fix isFetching/isUpdating for settings reducer

--- src/modules/settings/reducer.js
+++ src/modules/settings/reducer.js
@@ -13,16 +13,16 @@ export default (
         case FETCH_SETTINGS.REQUEST:
             return {...state, isFetching: true};
         case FETCH_SETTINGS.RECEIVE:
-            return {...state, data: action.data, error: ""};
+            return {...state, isFetching: false, data: action.data, error: ""};
         case FETCH_SETTINGS.ERROR:
-            return {...state, error: action.message};
+            return {...state, isFetching: false, error: action.message};
 
         case UPDATE_SETTINGS.REQUEST:
             return {...state, isUpdating: true};
         case UPDATE_SETTINGS.RECEIVE:
-            return {...state, data: action.data, error: ""};
+            return {...state, isUpdating: false, data: action.data, error: ""};
         case UPDATE_SETTINGS.ERROR:
-            return {...state, error: action.message};
+            return {...state, isUpdating: false, error: action.message};
 
         default:
             return state;
