cb02003571291c703e198a4ea7afc54c45a1a556
David Lougheed <david.lougheed@gmail.com>
2021-12-02 14:03:24 -0500

    fix: auth for adding assets

--- src/components/assets/AssetAddView.js
+++ src/components/assets/AssetAddView.js
@@ -1,16 +1,19 @@
 import React from "react";
 import {useHistory} from "react-router-dom";
 import {useDispatch} from "react-redux";
+import {useAuth0} from "@auth0/auth0-react";
 
 import {message, PageHeader} from "antd";
 
 import AssetForm from "./AssetForm";
 
+import {AUTH_AUDIENCE} from "../../config";
 import {addAsset} from "../../modules/assets/actions";
 
 const AssetAddView = () => {
     const dispatch = useDispatch();
     const history = useHistory();
+    const {getAccessTokenSilently} = useAuth0();
 
     const onFinish = async v => {
         console.log("adding asset", v);
@@ -19,11 +22,15 @@ const AssetAddView = () => {
         body.set("asset_type", v.asset_type)
         body.set("file", v.file);
 
-        const result = await dispatch(addAsset(body));
+        const accessToken = await getAccessTokenSilently({
+            audience: AUTH_AUDIENCE,
+            scope: "manage:content",
+        });
+        const result = await dispatch(addAsset(body, accessToken));
 
         if (!result.error) {
             message.success(`Added new asset: ${result.data.file_name}`);
-            history.push(`/assets/edit/${result.data.id}`);
+            history.replace(`/assets/edit/${result.data.id}`);
         }
     };
 
