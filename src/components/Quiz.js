5735728b09466ecabf459a0d1117a69f7fbc9f20
David Lougheed <david.lougheed@gmail.com>
2021-12-07 18:30:24 -0500

    feat: support matching quiz preview

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,11 +1,19 @@
 import React, {useState} from "react";
 
-import {Button, Card, Checkbox, Divider, Space, Typography} from "antd";
+import {Button, Card, Checkbox, Divider, Select, Space, Typography} from "antd";
 
 const Quiz = ({quiz_type, title, question, answer, options, ...props}) => {
   const [showAnswer, setShowAnswer] = useState(false);
-  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [selectedOptions, setSelectedOptions] = useState(
+    quiz_type === "match_values" ? [...new Array(options.length)] : [],
+  );
   const [correct, setCorrect] = useState(false);
+
+  // Used for matching quizzes
+  const allOptionAnswers = options.map(o => ({
+    value: o.answer.toString(),
+    label: o.answer.toString(),
+  })).sort((o1, o2) => o1.label.localeCompare(o2.label));
 
   return <Card title={title} {...props}>
     <Typography.Title level={5}>Question</Typography.Title>
@@ -14,7 +22,30 @@ const Quiz = ({quiz_type, title, question, answer, options, ...props}) => {
     <Divider />
 
     {quiz_type === "match_values" ? (<div>
-      TODO
+      {options.map((o, i) => (<div key={i}>
+        <Select
+          options={allOptionAnswers}
+          style={{
+            width: 80,
+            borderRadius: 2,
+            boxShadow: showAnswer
+              ? `0px 0px 8px ${o.answer === selectedOptions[i] ? "#52c41a" : "#ff4d4f"}`
+              : "none",
+          }}
+          value={selectedOptions[i]}
+          onChange={v => {
+            console.log(v)
+            setSelectedOptions(selectedOptions.map((v2, i2) => i === i2 ? v : v2))
+          }}
+        />
+        <span style={{marginLeft: "1em"}}>{o.label}</span>
+        <Divider />
+      </div>))}
+      <Button type="primary" onClick={() => {
+        console.log(selectedOptions);
+        setCorrect(options.reduce((acc, o, i) => acc && o.answer === selectedOptions[i], true));
+        setShowAnswer(true);
+      }}>Submit</Button>
     </div>) : null}
 
     {quiz_type === "select_all_that_apply" ? (<div>
