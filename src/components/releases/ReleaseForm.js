799706dc12e7db871fb28557888648cfdfd63e0b
David Lougheed <david.lougheed@gmail.com>
2022-09-19 20:47:20 -0400

    chore: switch release form to use ObjectForm

--- src/components/releases/ReleaseForm.js
+++ src/components/releases/ReleaseForm.js
@@ -1,42 +1,46 @@
-import React, {useMemo} from "react";
+import React from "react";
 import {useSelector} from "react-redux";
 
-import {Button, Col, Form, Input, Row, Switch} from "antd";
+import {Col, Form, Input, Row, Switch} from "antd";
 
-const RULES_REQUIRED = [{required: true}];
+import ObjectForm, {RULES_REQUIRED_BASIC, useObjectForm} from "../ObjectForm";
 
-const ReleaseForm = React.memo(({initialValues, onFinish, loading, ...props}) => {
-  const [form] = Form.useForm();
+const styles = {
+  columnPublished: {width: 200},
+};
 
+const transformInitialValues = v => ({
+  ...v,
+  published: !!v.published_dt,
+});
+
+const ReleaseForm = React.memo(({initialValues, ...props}) => {
   const releases = useSelector(state => state.releases.items);
+  const objectForm = useObjectForm({
+    initialValues,
+    transformInitialValues,
+    ...props,
+  });
 
-  const oldInitialValues = useMemo(() => initialValues ?? {}, [initialValues]);
-  console.log("initial values", oldInitialValues);
-  const newInitialValues = useMemo(() => ({
-    ...oldInitialValues,
-    published: !!oldInitialValues.published_dt,
-  }), [oldInitialValues]);
+  const {transformedInitialValues} = objectForm;
 
-  return <Form {...props} onFinish={onFinish} form={form} layout="vertical" initialValues={newInitialValues}>
+  return <ObjectForm objectForm={objectForm}>
     <Row gutter={12}>
       <Col flex={1}>
         <Form.Item label="Version">
-          <Input disabled={true} value={newInitialValues.version ?? ((releases[0]?.version ?? 0) + 1)} />
+          <Input disabled={true} value={transformedInitialValues.version ?? ((releases[0]?.version ?? 0) + 1)} />
         </Form.Item>
       </Col>
-      <Col style={{width: 200}}>
+      <Col style={styles.columnPublished}>
         <Form.Item label="Published">
-          <Switch checked={newInitialValues.published} />
+          <Switch checked={transformedInitialValues.published} />
         </Form.Item>
       </Col>
     </Row>
-    <Form.Item name="release_notes" label="Release Notes" rules={RULES_REQUIRED}>
-      <Input.TextArea rows={5} disabled={newInitialValues.published} />
+    <Form.Item name="release_notes" label="Release Notes" rules={RULES_REQUIRED_BASIC}>
+      <Input.TextArea rows={5} disabled={transformedInitialValues.published} />
     </Form.Item>
-    <Form.Item>
-      <Button type="primary" htmlType="submit" loading={loading}>Submit</Button>
-    </Form.Item>
-  </Form>;
+  </ObjectForm>;
 });
 
 export default ReleaseForm;
