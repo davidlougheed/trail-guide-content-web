c121f5a15885ec628fb2ea6920aff41187843c98
David Lougheed <david.lougheed@gmail.com>
2024-09-10 07:32:32 -0400

    fix: missing initialFetchDone settings reducer field

--- src/modules/settings/reducer.js
+++ src/modules/settings/reducer.js
@@ -2,6 +2,7 @@ import {FETCH_SETTINGS, UPDATE_SETTINGS} from "./actions";
 
 export default (
   state = {
+    initialFetchDone: false,
     isFetching: false,
     isUpdating: false,
     data: null,
@@ -13,7 +14,7 @@ export default (
     case FETCH_SETTINGS.REQUEST:
       return {...state, isFetching: true};
     case FETCH_SETTINGS.RECEIVE:
-      return {...state, isFetching: false, data: action.data, error: ""};
+      return {...state, isFetching: false, initialFetchDone: true, data: action.data, error: ""};
     case FETCH_SETTINGS.ERROR:
       return {...state, isFetching: false, error: action.message};
 
