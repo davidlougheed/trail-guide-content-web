ba6b828f70495aa2b6d0143eacf667cbed6d214a
David Lougheed <david.lougheed@gmail.com>
2024-10-14 08:42:53 -0400

    refact: check function for station actions

--- src/modules/stations/actions.ts
+++ src/modules/stations/actions.ts
@@ -1,6 +1,6 @@
 import {makeIfNeededAction, networkAction, networkActionTypes} from "../../utils";
 
-import type {AppDispatch, RootStateGetter} from "../../store";
+import type {AppDispatch, RootState, RootStateGetter} from "../../store";
 import {Station, StationUpdate} from "./types";
 
 export const FETCH_STATIONS = networkActionTypes("FETCH_STATIONS");
@@ -11,28 +11,25 @@ export const DELETE_STATION = networkActionTypes("DELETE_STATION");
 const fetchStations = networkAction<Station>(FETCH_STATIONS, "/stations");
 export const fetchStationsIfNeeded = makeIfNeededAction(fetchStations, "stations");
 
+const _check = (state: RootState)=> !(
+  state.stations.isFetching ||
+  state.stations.isAdding ||
+  state.stations.isUpdating ||
+  state.stations.isDeleting
+);
+
 const _addStation = networkAction<Station, Station>(ADD_STATION, "/stations", "POST");
 export const addStation = (body: Station, accessToken: string) =>
   (dispatch: AppDispatch, getState: RootStateGetter) => {
     const state = getState();
-    if (
-      state.stations.isFetching ||
-      state.stations.isAdding ||
-      state.stations.isUpdating ||
-      state.stations.isDeleting
-    ) return;
+    if (!_check(state)) return;
     return dispatch(_addStation(body, {}, accessToken));
   };
 
 export const updateStation = (stationID: string, stationData: StationUpdate, accessToken: string) =>
   (dispatch: AppDispatch, getState: RootStateGetter) => {
     const state = getState();
-    if (
-      state.stations.isFetching ||
-      state.stations.isAdding ||
-      state.stations.isUpdating ||
-      state.stations.isDeleting
-    ) return;
+    if (!_check(state)) return;
     return dispatch(networkAction<Station, StationUpdate>(UPDATE_STATION, `/stations/${stationID}`, "PUT")(
       stationData, {}, accessToken));
   };
@@ -40,12 +37,7 @@ export const updateStation = (stationID: string, stationData: StationUpdate, acc
 export const deleteStation = (stationID: string, accessToken: string) =>
   (dispatch: AppDispatch, getState: RootStateGetter) => {
     const state = getState();
-    if (
-      state.stations.isFetching ||
-      state.stations.isAdding ||
-      state.stations.isUpdating ||
-      state.stations.isDeleting
-    ) return;
+    if (!_check(state)) return;
     return dispatch(networkAction(DELETE_STATION, `/stations/${stationID}`, "DELETE")(
       {}, {id: stationID}, accessToken));
   };
