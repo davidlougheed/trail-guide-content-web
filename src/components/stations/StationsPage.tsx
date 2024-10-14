3d1b84e9fc7b362b1d14d50b83c508cc89cdadc0
David Lougheed <david.lougheed@gmail.com>
2024-10-14 08:10:17 -0400

    lint: StationsPage

--- src/components/stations/StationsPage.tsx
+++ src/components/stations/StationsPage.tsx
@@ -8,10 +8,10 @@ import StationListView from "./StationListView";
 import StationRevisionView from "./StationRevisionView";
 
 const StationsPage = React.memo(() => <Routes>
-  <Route path="list" element={<StationListView/>} />
-  <Route path="add" element={<StationAddView/>} />
+  <Route path="list" element={<StationListView />} />
+  <Route path="add" element={<StationAddView />} />
   <Route path="detail/:id" element={<StationDetailView />} />
-  <Route path="edit/:id" element={<StationEditView/>} />
+  <Route path="edit/:id" element={<StationEditView />} />
   <Route path="revision/:id" element={<StationRevisionView />} />
   <Route path="*" element={<Navigate to="list" replace={true} />} />
 </Routes>);
