c4dd2b1a9e6cb72d6d25ae98ceaa9fc958e1b9c5
David Lougheed <david.lougheed@gmail.com>
2024-10-14 08:21:53 -0400

    chore: delete category network actions

--- src/modules/categories/actions.ts
+++ src/modules/categories/actions.ts
@@ -3,6 +3,7 @@ import {makeIfNeededAction, networkAction, networkActionTypes} from "../../utils
 export const FETCH_CATEGORIES = networkActionTypes("FETCH_CATEGORIES");
 export const ADD_CATEGORY = networkActionTypes("ADD_CATEGORY");
 export const UPDATE_CATEGORY = networkActionTypes("UPDATE_CATEGORY");
+export const DELETE_CATEGORY = networkActionTypes("DELETE_CATEGORY");
 
 const fetchCategories = networkAction<string[]>(FETCH_CATEGORIES, "/categories");
 export const fetchCategoriesIfNeeded = makeIfNeededAction<string[]>(fetchCategories, "categories");
--- src/modules/categories/reducer.ts
+++ src/modules/categories/reducer.ts
@@ -1,7 +1,7 @@
 import {type GenericNetworkReducerState, makeGenericNetworkReducer} from "../../utils";
-import {ADD_CATEGORY, FETCH_CATEGORIES, UPDATE_CATEGORY} from "./actions";
+import {ADD_CATEGORY, FETCH_CATEGORIES, UPDATE_CATEGORY, DELETE_CATEGORY} from "./actions";
 import type {Category} from "./types";
 
 export type CategoryState = GenericNetworkReducerState<Category>;
 
-export default makeGenericNetworkReducer<Category>(FETCH_CATEGORIES, ADD_CATEGORY, UPDATE_CATEGORY);
+export default makeGenericNetworkReducer<Category>(FETCH_CATEGORIES, ADD_CATEGORY, UPDATE_CATEGORY, DELETE_CATEGORY);
