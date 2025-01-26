c6a8ebb9e1b164a478e53f2cfd03a1c1618e172e
David Lougheed <david.lougheed@gmail.com>
2025-01-26 09:01:55 -0500

    refact: rewrite PageQR as .tsx

--- src/components/pages/PageQR.tsx
+++ src/components/pages/PageQR.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, {CSSProperties} from "react";
 import {API_BASE_URL, APP_BASE_URL} from "../../config";
+import {Page} from "../../modules/pages/types";
 
-const styles = {
+const styles: Record<string, CSSProperties> = {
   img: {width: "100%", height: "auto"},
 };
 
-const PageQR = React.memo(({page}) => {
+const PageQR = React.memo(({page}: {page: Page}) => {
   if (!page) return <div />;
   const pageURL = `${APP_BASE_URL}/pages/${page.id}`;
   return <div>
