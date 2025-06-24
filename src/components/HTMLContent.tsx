d204a679c53abe7b8e4236be927cf2b5d1bdab66
David Lougheed <david.lougheed@gmail.com>
2025-06-24 14:21:56 -0400

    chore: update copyright date to 2025 across codebase

--- src/components/HTMLContent.tsx
+++ src/components/HTMLContent.tsx
@@ -1,5 +1,5 @@
 // A web interface to manage a trail guide mobile app's content and data.
-// Copyright (C) 2021-2024  David Lougheed
+// Copyright (C) 2021-2025  David Lougheed
 // See NOTICE for more information.
 
 import React, {useCallback, useEffect, useMemo, useState} from "react";
